2489288e94480e393ab1815196b721b9bc5a42c5
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-31 14:30:43 +0530

    user's choice

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #Radhe Radhe
 import demoji
 
-text=input("Enter you text with emojis:")
-output=demoji.findall(text)
+choice='y'
+while(choice=='y'):
+         text=input("Enter you text with emojis:")
+         output=demoji.findall(text)
+         print(output)
 
-print(output)
+
+         choice=input("Do you want to continue?:(y/n):")
+         if(choice=='y'):
+               continue
+         else:
+               break
+         
+         
